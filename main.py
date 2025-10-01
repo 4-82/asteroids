92884f440c12678c8b049b4221103a426357bfa7
4-82 <180662930+4-82@users.noreply.github.com>
2025-10-01 06:44:10 -0400

    main: add clock, dt

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     pygame.init()
+    clock = pygame.clock.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0 
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,8 @@ def main():
 
         screen.fill("#000000")
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
